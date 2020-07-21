a42f8041886d53004f6ea96b59252aceff9a89e2
hassannaveed24 <hassannaveed24@gmail.com>
2020-07-21 10:49:10 +0000

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -1,29 +1,29 @@
-exports.test = (async (client) => {
+// exports.test = (async (client) => {
 
 
-    console.log(client)
-    client.on('testSocket', (req, res) => {
-        console.log('Hello console.');
-        res.send('hello');
-    })
+//     console.log(client)
+//     client.on('testSocket', (req, res) => {
+//         console.log('Hello console.');
+//         res.send('hello');
+//     })
 
-    // Setting Threshold by property manager
-    client.on('saveThreshold', async req => {
-        try {
-            let threshold = new threshold_Schema(req.body);
-            await threshold.save();
-            threshold = await threshold_Schema.find();
-            client.emit('broadcastThreshold', threshold);
-        } catch (err) {
-            console.log(err);
-        }
-    })
-    client.emit('test123', "degege");
+//     // Setting Threshold by property manager
+//     client.on('saveThreshold', async req => {
+//         try {
+//             let threshold = new threshold_Schema(req.body);
+//             await threshold.save();
+//             threshold = await threshold_Schema.find();
+//             client.emit('broadcastThreshold', threshold);
+//         } catch (err) {
+//             console.log(err);
+//         }
+//     })
+//     client.emit('test123', "degege");
 
 
-    // Disconnecting Socket
-    client.on("disconnect", () => {
-        console.log("Socket disconnected")
-    })
-})
+//     // Disconnecting Socket
+//     client.on("disconnect", () => {
+//         console.log("Socket disconnected")
+//     })
+// })
 
